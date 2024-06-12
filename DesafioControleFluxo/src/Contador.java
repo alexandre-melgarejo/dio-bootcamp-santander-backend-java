d852868a2414cafb0ed3f66bf89f6830ce46d865
Alexandre Melgarejo <ar.melgarejo@gmail.com>
2024-06-12 01:29:39 -0300

    feat: implementacao de loop para reiniciar o Contador

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,28 +1,50 @@
-
 // Desafio Controle de Fluxo: Contador
 
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 public class Contador {
     public static void main(String[] args) throws Exception {
 
         int parametroUm;
         int parametroDois;
+        String reinicia = "S";
 
         try (Scanner terminal = new Scanner(System.in)) {
 
-            System.out.println("Digite o primeiro parâmetro:");
-            parametroUm = terminal.nextInt();
-            
-            System.out.println("Digite o segundo parâmetro:");
-            parametroDois = terminal.nextInt();
+            // Laço para executar indefinidamente
+            do {
 
-            contar(parametroUm, parametroDois);
+                System.out.println("Bootcamp DIO Santander 2024 - Backend Java");
+                System.out.println("Desafio Controle de Fluxo: Contador");
+                System.out.println("------------------------------------------");
+                System.out.println("[Pressione Ctrl+C para sair]\n");
+
+                System.out.println("Digite o primeiro parâmetro:");
+                parametroUm = terminal.nextInt();
+            
+                System.out.println("Digite o segundo parâmetro:");
+                parametroDois = terminal.nextInt();
+
+                System.out.println("-------");
+                contar(parametroUm, parametroDois);
+                System.out.println("-------");
+
+                System.out.println("Deseja executar novamente (S/N) ?");
+                reinicia = terminal.next();
+
+            } while (reinicia.equalsIgnoreCase("S"));
+
+            // Encerrado normalmente
+            System.out.println("\nObrigado por Utilizar o Contador!\n");
 
         } catch (ParametrosInvalidosException e) {
             System.out.println("\nO segundo parâmetro deve ser maior que o primeiro.\n");
+        } catch (NoSuchElementException e) {
+            System.out.println("\nObrigado por Utilizar o Contador!\n");
+        } catch (Exception e) {
+            System.out.println("Falha crítica: " + e.getMessage());
         }
-
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
@@ -32,14 +54,10 @@ public class Contador {
         }
 
         final String TEXTO_EXIBIR_CONTAGEM = "Imprimindo o número %d";
-
         int contagem = parametroDois - parametroUm;
 
         for (int x = 1; x <= contagem; x++) {
-
             System.out.println(String.format(TEXTO_EXIBIR_CONTAGEM, x));
-
         }
-
     }
 }
