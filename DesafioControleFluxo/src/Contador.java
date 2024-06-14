5a7119e00062cfe504dadc40260c8d4dbe98e1e2
Alexandre Melgarejo <ar.melgarejo@gmail.com>
2024-06-13 21:07:56 -0300

    feat: implementada execucao continua do Contador

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -8,13 +8,13 @@ public class Contador {
 
         int parametroUm;
         int parametroDois;
-        String reinicia = "S";
 
         try (Scanner terminal = new Scanner(System.in)) {
 
             // Laço para executar indefinidamente
-            do {
+            while (true) {
 
+                System.out.println("\n------------------------------------------");
                 System.out.println("Bootcamp DIO Santander 2024 - Backend Java");
                 System.out.println("Desafio Controle de Fluxo: Contador");
                 System.out.println("------------------------------------------");
@@ -26,17 +26,11 @@ public class Contador {
                 System.out.println("Digite o segundo parâmetro:");
                 parametroDois = terminal.nextInt();
 
-                System.out.println("-------");
-                contar(parametroUm, parametroDois);
-                System.out.println("-------");
+                System.out.println("--");
+                imprimirContagem(contar(parametroUm, parametroDois));
+                System.out.println("--");
 
-                System.out.println("Deseja executar novamente (S/N) ?");
-                reinicia = terminal.next();
-
-            } while (reinicia.equalsIgnoreCase("S"));
-
-            // Encerrado normalmente
-            System.out.println("\nObrigado por Utilizar o Contador!\n");
+            }
 
         } catch (ParametrosInvalidosException e) {
             System.out.println("\nO segundo parâmetro deve ser maior que o primeiro.\n");
@@ -47,16 +41,17 @@ public class Contador {
         }
     }
 
-    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+    static int contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         // Valida regra dos parametros (parametroUm não pode ser maior do que parametroUm)
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
+        return (parametroDois - parametroUm);
+    }
 
+    static void imprimirContagem(int iteracoes) {
         final String TEXTO_EXIBIR_CONTAGEM = "Imprimindo o número %d";
-        int contagem = parametroDois - parametroUm;
-
-        for (int x = 1; x <= contagem; x++) {
+        for (int x = 1; x <= iteracoes; x++) {
             System.out.println(String.format(TEXTO_EXIBIR_CONTAGEM, x));
         }
     }
